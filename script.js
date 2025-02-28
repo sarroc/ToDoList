14bed6587b0464c2813099e22344d21cc1dcc4a0
Sarah Rocco <sarahrocco90@gmail.com>
2025-02-28 19:39:07 +0100

    create function in javaScript to create new list items and remove them

---
+++ script.js
@@ -0,0 +1,6 @@
+function handleClick() {
+  const list = document.querySelector("ul");
+  const input = document.querySelector("input");
+
+  const listItem = document.createElement("li");
+  const btn = document.createElement("button");
